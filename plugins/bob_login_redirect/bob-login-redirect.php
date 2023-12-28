f5b508b71449191ef4e0bfcd57bab86f5051ff08
Bob Bloom <info@southlasalle.com>
2023-12-27 23:54:10 -0500

    Update bob-login-redirect.php

--- plugins/bob_login_redirect/bob-login-redirect.php
+++ plugins/bob_login_redirect/bob-login-redirect.php
@@ -20,6 +20,30 @@ if ( !defined( 'ABSPATH' ) ) exit;
  * Login redirect to user specific URL.
  */
 function bob_login_redirect( $user ) {
+    if ( ! is_user_logged_in() ) {
+        
+        if ( is_admin() ) {
+             wp_redirect("https://ra-docs.creativeraven.com/wp-admin/");
+             exit;
+        } else {
+             wp_redirect("https://ra-docs.creativeraven.com/");
+             exit;
+        }
+    }
+
+    if ( is_user_logged_in() }
+
+
+            
+
+        
+   wp_redirect("https://ra-docs.creativeraven.com/wp-admin/");
+  exit;
+  } else {
+  wp_redirect("https://ra-docs.creativeraven.com/wp-admin/");
+  exit;
+ }
+
     // do something
 
     // https://developer.wordpress.org/reference/functions/wp_redirect/
@@ -32,6 +56,32 @@ function bob_login_redirect( $user ) {
     return;
 }
 add_filter( 'login_redirect', 'bob_login_redirect' );
+
+
+/**
+ * Redirect user after successful login.
+ *
+ * @param string $redirect_to URL to redirect to.
+ * @param string $request URL the user is coming from.
+ * @param object $user Logged user's data.
+ * @return string
+ */
+function my_login_redirect( $redirect_to, $request, $user ) {
+	//is there a user to check?
+	if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+		//check for admins
+		if ( in_array( 'administrator', $user->roles ) ) {
+			// redirect them to the default place
+			return $redirect_to;
+		} else {
+			return home_url();
+		}
+	} else {
+		return $redirect_to;
+	}
+}
+
+add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
 
 /**
  * Logout redirect to user specific URL.
