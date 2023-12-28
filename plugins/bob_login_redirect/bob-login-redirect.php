d643f1bed0a9263609065022ce947d8767f22920
Bob Bloom <info@southlasalle.com>
2023-12-28 00:26:44 -0500

    Update bob-login-redirect.php

--- plugins/bob_login_redirect/bob-login-redirect.php
+++ plugins/bob_login_redirect/bob-login-redirect.php
@@ -26,16 +26,23 @@ if ( !defined( 'ABSPATH' ) ) exit;
  * @return string
  */
 function bob_login_redirect( $redirect_to, $request, $user ) {
+	
 	//is there a user to check?
 	if ( isset( $user->roles ) && is_array( $user->roles ) ) {
-		//check for admins
+		
+		// if are the administrator AND are logging into the wp-admin, then continue into the wp-admin
+		// https://developer.wordpress.org/reference/functions/is_admin/
 		if ( in_array( 'administrator', $user->roles ) ) {
-			// redirect them to the default place
+			// redirect to the default url
 			return $redirect_to;
 		} else {
+			// not an administrator OR are an administrator who is logging in via the front-end
+			// redirect to the front-end's home page
 			return home_url();
 		}
+		
 	} else {
+		// there is no user
 		return $redirect_to;
 	}
 }
