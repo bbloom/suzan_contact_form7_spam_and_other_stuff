573d5f5a50d3707fda583bae61a07c6acddc0f87
Bob Bloom <info@southlasalle.com>
2023-12-27 21:47:04 -0500

    Update and rename bob_login_redirect.php to bob-login-redirect.php

---
+++ plugins/bob_login_redirect/bob-login-redirect.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Bob Custom Redirection Plugin for Login and Logout
+Plugin URI: https://github.com/bbloom/suzan_contact_form7_spam_and_other_shit
+Description: This plugin which enables you to redirect users to specific URL on login or logout or both.
+Version: 1.0
+Author: Bob Bloom
+Author URI: https://lasallesoftware.ca
+License: GPLv2 or later
+Text Domain: bob-login-redirect
+Domain Path: /
+*/
+
+/**
+ * Restrict this file to call directly
+*/
+if ( !defined( 'ABSPATH' ) ) exit;
+
