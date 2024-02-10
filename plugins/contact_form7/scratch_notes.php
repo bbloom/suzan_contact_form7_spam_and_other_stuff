b9dc26e340d4745d54db6e1aa4b9692492d8c42c
Bob Bloom <bob.bloom@lasallesoftware.ca>
2024-02-10 16:47:25 -0500

    Create scratch_notes.php

---
+++ plugins/contact_form7/scratch_notes.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+SCRATCH NOTES
+--> will end up deleting this file
+
+* create a PHP for the Cloudways ../public_html/custom_cron_scripts/ folder (that should already be there!)
+* add the PHP file
+* move the files for this plugin from this repo's root to this subfolder
+
+
+=================================================================================================================================================
+I want to add a file that is "wget", or otherwise read, or perhaps an easy API call, to indicate if the wget updates to the files should happen
+
