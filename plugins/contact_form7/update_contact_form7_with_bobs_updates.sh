7d6bb1fad4bdf53ab6edeb3d9d75c0e7063a22ed
Bob Bloom <bob.bloom@lasallesoftware.ca>
2024-02-10 23:03:03 -0500

    Create update_contact_form7_with_bobs_updates.sh

---
+++ plugins/contact_form7/update_contact_form7_with_bobs_updates.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# this file resides on Cloudways servers at ../public_html/custom_cron_scripts/ folder (should already be there from Akeeba install!)
+# it is expected that this file is invoked via CRON
+
+# =============================================================================================
+#  STEP 1: should we update the Contact Form7 plugin with Bob's updates?
+# =============================================================================================
+
+# -O- = return contents, do not actually download
+# -q  = suppress wget's progress output
+do_update = wget -O- -q https://raw.githubusercontent.com/bbloom/suzan_contact_form7_spam_and_other_shit/master/plugins/contact_form7/is_update_contact_form7_with_bobs_updates.txt
